89585dda63b4ff04bfd973e14b1c496182c5fcf2
Abdullah Alqahtani <abdalqa6@gmail.com>
2020-10-02 16:04:40 +0300

    Coroutine error and exception supervisor job

--- app/src/main/java/com/abdull/coroutinestutorial/MainActivity.kt
+++ app/src/main/java/com/abdull/coroutinestutorial/MainActivity.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.Dispatchers.IO
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         private const val TAG = "MainActivity"
     }
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_coroutine.setOnClickListener{
+        button_coroutine.setOnClickListener {
             main()
         }
 
@@ -27,70 +27,64 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    val handler = CoroutineExceptionHandler{_, exception ->
-        println("Exception thrown in one of the children: $exception") }
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("Exception thrown in one of the children: $exception")
+    }
 
-    fun main(){
+    fun main() {
         val parentJob = CoroutineScope(IO).launch(handler) {
-            val jobA = launch {
-                val resultA = getResult(1)
-                println("resultA $resultA")
-            }
-            jobA.invokeOnCompletion {
-                t ->
-                if(t != null){
-                    println("Error getting resultA: $t")
+            supervisorScope {
+                val jobA = launch {
+                    val resultA = getResult(1)
+                    println("resultA $resultA")
+                }
+                jobA.invokeOnCompletion { t ->
+                    if (t != null) {
+                        println("Error getting resultA: $t")
+                    }
+                }
+                val jobB = launch {
+                    val resultB = getResult(2)
+                    println("resultB $resultB")
+                }
+                jobB.invokeOnCompletion { t ->
+                    if (t != null) {
+                        println("Error getting resultB: $t")
+                    }
+                }
+                val jobC = launch {
+                    val resultC = getResult(3)
+                    println("resultC $resultC")
+                }
+                jobC.invokeOnCompletion { t ->
+                    if (t != null) {
+                        println("Error getting resultC: $t")
+                    }
                 }
             }
-            val jobB = launch {
-                val resultB = getResult(2)
-                println("resultB $resultB")
-            }
-            jobB.invokeOnCompletion {
-                t ->
-                if(t != null){
-                    println("Error getting resultB: $t")
-                }
-            }
-            val jobC = launch {
-                val resultC = getResult(3)
-                println("resultC $resultC")
-            }
-            jobC.invokeOnCompletion {
-                t ->
-                if(t != null){
-                    println("Error getting resultC: $t")
-                }
-            }
-
         }
         parentJob.invokeOnCompletion { t ->
-            if(t != null){
+            if (t != null) {
                 println("Parent job failed: $t")
-            }
-            else{
+            } else {
                 println("Parent job SUCCESS")
             }
         }
     }
 
 
-
-    suspend fun getResult(number : Int) : Int{
+    suspend fun getResult(number: Int): Int {
         println("Result number $number running on thread: ${Thread.currentThread().name}")
-        delay(number*500L)
-        if(number == 2){
-//            throw Exception("Error getting result for number $number")
-//            cancel(CancellationException("Error getting result for number $number")) // calling cancel from the suspend method won't do anything
-            throw CancellationException("Error getting result for number $number") //this is equivalent to calling cancel() on the job its self
+        delay(number * 500L)
+        if (number == 2) {
+            throw Exception("Error getting result for number $number")
         }
-        return number*2
+        return number * 2
     }
 
-    private fun println(message: String){
+    private fun println(message: String) {
         Log.d(TAG, " $message")
     }
-
 
 
 }
